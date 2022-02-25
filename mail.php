c51d7f76186868d08a2884cb718d25e70cfa0f04
Bastien TAKIS <bastientakis@gmail.com>
2022-02-25 13:05:43 +0100

    Projet Final (normalement)

--- mail.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$to = $_POST['email'];
-$subject = $_POST['sujet'];
-$message = $_POST['message'];
-$message = wordwrap($message, 70, "\r\n");
-mail($to,$subject,$message);
-header('Location: ./');
